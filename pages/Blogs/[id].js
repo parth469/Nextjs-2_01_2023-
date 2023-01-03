8fb8bc1c1ac462f9c30ae887ad65daaee8baf432
parth469 <parthmain2001@gmail.com>
2023-01-03 17:59:44 +0530

    new add

--- pages/Blogs/[id].js
+++ pages/Blogs/[id].js
@@ -1,58 +1,38 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 
-// export const getStaticProps = async () => {
-//   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-//   const json = await res.json();
-//   return {
-//     props: { data: json },
-//   };
-// };
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
 
-export default function Alluserdata(){
-    const {query} = useRouter()
-    console.log(query.id)
-    return (
-        <>
-        <h1>This is user {query.id}data</h1>
-        </>
-    )
+  const paths = data.map((user) => {
+    return {
+      params: {
+        id: user.id.toString(),
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/` + id);
+  const data = await res.json();
+  return {
+    props: { user: data },
+  };
+};
+export default function Alluserdata({user}) {
+  const { query } = useRouter();
+  console.log(query.id,
+    user.email,
+    user.name,
+    user.phone,);
+  return (
+    <>
+      <h1>This is user {query.id}data</h1>
+    </>
+  );
 }
-
-
-// import Link from "next/link";
-
-
-// export default function Blog_1({ data }) {
-//   return (
-//     <>
-//       <ul>
-//         <li>
-//           <Link href="/Blogs/Blogs_1">Blog 1</Link>
-//         </li>
-//         <li>
-//           <Link href="/Blogs/Blogs_2">Blog 2</Link>
-//         </li>
-//       </ul>
-//       <ol>
-//         <Link href="/Blogs/*">Blog 2</Link>
-//       </ol>
-//       <h1>ALL Website userdata</h1>
-//       <br />
-    
-//       {data.map((datas) => (
-//         <div key={datas.id}>
-//           <ol>
-//             <h2>Name :- {`${datas.name} ${datas.username}`}</h2>
-//             <h3>Email address :- {datas.email}</h3>
-//             <h3>Phone Number :- {datas.phone}</h3>
-//             <h4>
-//               Address :-
-//               {`${datas.address.suite}, ${datas.address.street}, ${datas.address.city}, ${datas.address.zipcode}`}
-//             </h4>
-//             <br />
-//           </ol>
-//         </div>
-//       ))}
-//     </>
-//   );
-// }
